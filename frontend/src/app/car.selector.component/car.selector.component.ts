6791b360edd8f2bf9e23b2deec192059be0cd2e5
DavidMoure <david.cerqueira@deus.ai>
2026-05-04 12:47:27 +0200

    feat/separacion de logica de carselector de la cascada

---
+++ frontend/src/app/car.selector.component/car.selector.component.ts
@@ -0,0 +1,94 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+  output,
+  signal,
+} from '@angular/core';
+import { ItemDatalistCarselectorComponent } from '../componentes/item.datalist.carselector.component/item.datalist.carselector.component';
+import { Brand } from '../interfaces/cars/brand';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
+import { CascadeService } from '../services/cascade.service';
+import { FormatVersionPipe } from '../pipes/format.version.pipe';
+import { Version } from '../interfaces/cars/version';
+
+@Component({
+  selector: 'car-selector-component',
+  imports: [ItemDatalistCarselectorComponent, FormatVersionPipe],
+  templateUrl: './car.selector.component.html',
+  styleUrl: './car.selector.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [FormatVersionPipe],
+})
+export class CarSelectorComponent {
+  cascadeService = inject(CascadeService);
+  formatVersion = inject(FormatVersionPipe);
+  dataId = Math.random().toString(36).substring(2, 9);
+
+  brands = input.required<Brand[]>();
+  query = signal<string>('');
+  selectedBrandId = signal<number | null>(null);
+  selectedModelId = signal<number | null>(null);
+  idEmitter = output<number>();
+  models = computed(() => this.modelResource.value() ?? []);
+  versions = computed(() => this.versionResource.value() ?? []);
+  version = signal<Version | null>(null);
+  modelResource = rxResource({
+    params: () => ({ id: this.selectedBrandId() }),
+    stream: ({ params }) => {
+      if (!params.id) {
+        return of([]);
+      }
+      return this.cascadeService.getModels(params.id);
+    },
+  });
+
+  onSelectedBrand(brandId: number) {
+    if (brandId === 0) {
+      this.selectedBrandId.set(null);
+      return;
+    }
+    this.selectedBrandId.set(brandId);
+  }
+  onSelectedModel(modelId: number) {
+    this.selectedModelId.set(modelId);
+  }
+  versionResource = rxResource({
+    params: () => ({ id: this.selectedModelId() }),
+    stream: ({ params }) => {
+      if (!params.id) {
+        return of([]);
+      }
+      return this.cascadeService.getVersions(params.id);
+    },
+  });
+
+  onSearchInput(event: Event) {
+    const queryData = event.target as HTMLInputElement;
+    this.query.set(queryData.value);
+  }
+  onSelectVersion(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.validateAndLoad(input.value);
+  }
+  onEnterPressed(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.validateAndLoad(input.value);
+    input.blur();
+  }
+  private validateAndLoad(value: string) {
+    const selectedName = value.trim();
+    const versionFound = this.versions().find(
+      (version) => this.formatVersion.transform(version) === selectedName,
+    );
+    if (versionFound) {
+      this.version.set(versionFound);
+      this.idEmitter.emit(versionFound.id);
+      return;
+    }
+    this.version.set(null);
+  }
+}
--- frontend/src/app/componentes/cascade.component/cascade.component.ts
+++ frontend/src/app/componentes/cascade.component/cascade.component.ts
@@ -13,37 +13,28 @@ import {
 import { Brand } from '../../interfaces/cars/brand';
 import { Disc } from '../../interfaces/disc/disc';
 import { CascadeService } from '../../services/cascade.service';
-
 import { DiscTheme } from '../../styles/discThemes';
-import { ItemDatalistCarselectorComponent } from '../item.datalist.carselector.component/item.datalist.carselector.component';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { of } from 'rxjs';
 import { FormatVersionPipe } from '../../pipes/format.version.pipe';
-import { Version } from '../../interfaces/cars/version';
 import { DiscApiService } from '../../services/disc.api.service';
+import { CarSelectorComponent } from '../../car.selector.component/car.selector.component';
 
 @Component({
   selector: 'cascade-component',
-  imports: [ItemDatalistCarselectorComponent, FormatVersionPipe],
+  imports: [CarSelectorComponent],
   templateUrl: './cascade.component.html',
   styleUrl: './cascade.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [FormatVersionPipe],
 })
 export class CascadeComponent {
-  formatVersion = inject(FormatVersionPipe);
   theme = input.required<DiscTheme>();
   disc = model<Disc>();
   cascadeService = inject(CascadeService);
   discApiService = inject(DiscApiService);
   brands = signal<Brand[]>(this.cascadeService.brands());
-  query = signal<string>('');
-  selectedBrandId = signal<number | null>(null);
-  selectedModelId = signal<number | null>(null);
-  models = computed(() => this.modelResource.value() ?? []);
-  versions = computed(() => this.versionResource.value() ?? []);
-  version = signal<Version | null>(null);
-  dataId = Math.random().toString(36).substring(2, 9);
+  idVersion = signal<number | null>(null);
   position = signal<'front' | 'rear'>('front');
   discOptions = computed(() => this.discResource.value());
   constructor() {
@@ -58,37 +49,9 @@ export class CascadeComponent {
       }
     });
   }
-  modelResource = rxResource({
-    params: () => ({ id: this.selectedBrandId() }),
-    stream: ({ params }) => {
-      if (!params.id) {
-        return of([]);
-      }
-      return this.cascadeService.getModels(params.id);
-    },
-  });
 
-  onSelectedBrand(brandId: number) {
-    if (brandId === 0) {
-      this.selectedBrandId.set(null);
-      return;
-    }
-    this.selectedBrandId.set(brandId);
-  }
-  onSelectedModel(modelId: number) {
-    this.selectedModelId.set(modelId);
-  }
-  versionResource = rxResource({
-    params: () => ({ id: this.selectedModelId() }),
-    stream: ({ params }) => {
-      if (!params.id) {
-        return of([]);
-      }
-      return this.cascadeService.getVersions(params.id);
-    },
-  });
   discResource = rxResource({
-    params: () => ({ idVersion: this.version()?.id }),
+    params: () => ({ idVersion: this.idVersion() }),
     stream: ({ params }) => {
       if (!params.idVersion) {
         return of([]);
@@ -97,29 +60,8 @@ export class CascadeComponent {
     },
   });
 
-  onSearchInput(event: Event) {
-    const queryData = event.target as HTMLInputElement;
-    this.query.set(queryData.value);
-  }
-  onSelectVersion(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.validateAndLoad(input.value);
-  }
-  onEnterPressed(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.validateAndLoad(input.value);
-    input.blur();
-  }
-  private validateAndLoad(value: string) {
-    const selectedName = value.trim();
-    const versionFound = this.versions().find(
-      (version) => this.formatVersion.transform(version) === selectedName,
-    );
-    if (versionFound) {
-      this.version.set(versionFound);
-      return;
-    }
-    this.version.set(null);
+  onVersionIDReceiver(id: number) {
+    this.idVersion.set(id);
   }
 
   togglePosition(pos: 'front' | 'rear') {
