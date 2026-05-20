5edc15d183eb2356fa8aec2b59a9861e3f46c924
DavidMoure <david.cerqueira@deus.ai>
2026-05-20 16:55:27 +0200

    feat: unificacion de estilos y simplificado de paginas auxiliares

--- frontend/src/app/pages/forbidden.page.component/forbidden.page.component.ts
+++ frontend/src/app/pages/forbidden.page.component/forbidden.page.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forbidden-page-component',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './forbidden.page.component.html',
   styleUrl: './forbidden.page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- frontend/src/app/pages/maintenance.page.component/maintenance.page.component.ts
+++ frontend/src/app/pages/maintenance.page.component/maintenance.page.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'maintenance-page-component',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './maintenance.page.component.html',
   styleUrl: './maintenance.page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
